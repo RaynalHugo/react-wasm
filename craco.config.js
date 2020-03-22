9e5b196a5059b7c1b3d66822b158c40138fe31b6
Hugo Raynal <hugo.raynal@sterblue.com>
2020-03-22 23:53:48 +0100

    remove logs in craco

--- craco.config.js
+++ craco.config.js
@@ -3,26 +3,22 @@
 };
 module.exports = {
   webpack: {
-    configure: webpackConfig => (
-      console.log(webpackConfig),
-      console.log(webpackConfig.module.rules),
-      {
-        ...webpackConfig,
-        module: {
-          ...webpackConfig.module,
-          rules: [
-            {
-              test: /\.wasm$/,
-              type: "javascript/auto"
-            },
-            {
-              test: /\.worker$/,
-              use: { loader: "worker-loader" }
-            },
-            ...webpackConfig.module.rules
-          ]
-        }
+    configure: webpackConfig => ({
+      ...webpackConfig,
+      module: {
+        ...webpackConfig.module,
+        rules: [
+          {
+            test: /\.wasm$/,
+            type: "javascript/auto"
+          },
+          {
+            test: /\.worker$/,
+            use: { loader: "worker-loader" }
+          },
+          ...webpackConfig.module.rules
+        ]
       }
-    )
+    })
   }
 };
